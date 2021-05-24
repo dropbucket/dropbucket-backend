21fefb61a083daf2202b06c366497c042eb57fec
ZION <sju0924@khu.ac.kr>
2021-05-24 23:24:58 +0900

    공유 상태 변경
    shareService.js 추가
    SetShare 호출시 is_shared 변경

--- src/routes/filesRoute.js
+++ src/routes/filesRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { findItem, updateItem } from '../controllers/filesController.js';
+import {SetShare} from '../services/shareService.js'
 
 const router = express.Router();
 
 router.get('/item', findItem);     // 폴더 & 파일 조회
 router.patch('/item', updateItem); // 폴더 & 파일 수정
+router.patch('/share',SetShare)
 
 export default router;
---
+++ src/services/shareService.js
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+const AWS = require("aws-sdk");
+dotenv.config();
+const BUCKET_NAME = "dropbucketpractice";
+var table = "FileDirTable";
+const s3 = new AWS.S3();
+var docClient = new AWS.DynamoDB.DocumentClient()
+
+export const SetShare = async (req) => {
+    console.log(req.body);
+    const id = req.body.id;
+    const parent_id = req.body.parent_id;
+
+    var params = {
+        TableName:table,
+        Key:{
+            "id": id,
+            "parent_id": parent_id
+        },
+        UpdateExpression: "set is_folder = :i"
+    };
+
+    let data = await docClient.get(params).promise();
+    let is_shared = data.Item.is_shared;
+
+    params = {
+        TableName: 'FileDirTable',
+        Key: {
+          parent_id: req.body.parent_id,
+          id: req.body.id,
+        },
+        UpdateExpression: 'set is_shared = :i',
+        ExpressionAttributeValues: {
+          ':i': !is_shared,
+        },
+        ReturnValues: 'UPDATED_NEW',
+      };
+
+    console.log("Set share");
+    docClient.update(params, function(err, data) {
+        if (err) {
+            console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+        }
+    }); 
+}
